b8722368d6633a4011d5682853aa5da8ae8830b6
Ayushhgi <goyal.ayush2424@gmail.com>
2025-05-29 21:31:48 +0530

    change in admissionDropdown.jsx

---
+++ frontend/project/src/components/AdmissionDropdown.jsx
@@ -0,0 +1,9 @@
+export default function AdmissionDropdown() {
+  return (
+    <div className="bg-white shadow-md rounded-md p-4">
+      <div className="py-1 hover:bg-gray-100 px-3 cursor-pointer">B.Tech</div>
+      <div className="py-1 hover:bg-gray-100 px-3 cursor-pointer">M.Tech</div>
+      <div className="py-1 hover:bg-gray-100 px-3 cursor-pointer">Ph.D</div>
+    </div>
+  );
+}
